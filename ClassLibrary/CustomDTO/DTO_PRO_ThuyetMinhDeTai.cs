540d6788d66d80a0c898803c9c1d73dae7793fd6
DESKTOP-G3TO0Q2\NHC <hung.hoang@gosmartlog.com>
2020-02-04 16:43:19 +0700

    update ThuyetMinhDeTai

--- ClassLibrary/CustomDTO/DTO_PRO_ThuyetMinhDeTai.cs
+++ ClassLibrary/CustomDTO/DTO_PRO_ThuyetMinhDeTai.cs
@@ -10,31 +10,30 @@ namespace DTOModel
     {
         public List<DTO_PRO_ThuyetMinhDeTai_NhanLucNghienCuu> ListNhanLucNghienCuu { get; set; }
         public List<DTO_PRO_ThuyetMinhDeTai_GioiThieuChuyenGia> ListGioiThieuChuyenGia { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThapJson CacBienSoCanThuThap { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien KeHoachThucHien { get; set; }
-    }
-
-    public class DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThapJson
-    {
-        public List<DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThap> ListBienSoNen { get; set; }
-        public List<DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThap> ListBienSoDocLap { get; set; }
-        public List<DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThap> ListBienSoPhuThuoc { get; set; }
+        public List<DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien> ListKeHoachThucHien { get; set; }
+        public List<DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThap> ListBienSo { get; set; }
     }
 
     public class DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien
     {
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet VietDeCuong { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet ThongQuaHDKH { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet ThuThapSoLieu { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet PhanTichSoLieuGiuaKy { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet PhanTichSoLieuCuoiCung { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet VietBaiDangBaoTrongNuoc { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet NghiemThuDeTai { get; set; }
-        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet VietBaiDangBaoQuocTe { get; set; }
-    }
+        public static string VietDeCuong = "Viết đề cương";
+        public static string ThongQuaHDKH = "Thông qua Hội đồng khoa học và Hội đồng đạo đức";
+        public static string ThuThapSoLieu = "Thu thập số liệu";
+        public static string PhanTichSoLieuGiuaKy = "Phân tích số liệu giữa kỳ";
+        public static string PhanTichSoLieuCuoiCung = "Phân tích số liệu cuối cùng";
+        public static string VietBaiDangBaoTrongNuoc = "Viết bài đăng báo (Tạp chí trong nước)";
+        public static string NghiemThuDeTai = "Nghiệm thu đề tài";
+        public static string VietBaiDangBaoQuocTe = "Viết bài đăng báo (tạp chí quốc tế)";
 
-    public class DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien_ChiTiet
-    {
+        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien()
+        {
+
+        }
+        public DTO_PRO_ThuyetMinhDeTai_KeHoachThucHien(string noidung)
+        {
+            this.NoiDung = noidung;
+        }
+
         public string NoiDung { get; set; }
         public string ThoiGianThucHien { get; set; }
         public string DuKienKetQua { get; set; }
@@ -60,8 +59,20 @@ namespace DTOModel
 
     public class DTO_PRO_ThuyetMinhDeTai_CacBienSoCanThuThap
     {
+        public enum BienSo
+        {
+            BSNen = 0,
+            BSDocLap = 1,
+            BSPhuThuoc = 2
+        }
         public string Ten { get; set; }
         public string PhanLoai { get; set; }
+        /// <summary>
+        /// 0:Biến số nền
+        /// 1:Biến số độc lập
+        /// 2:Biến phụ thuộc
+        /// </summary>
+        public int LoaiBienSo { get; set; }
         public string GiaTri { get; set; }
         public string CachThuThap { get; set; }
     }
