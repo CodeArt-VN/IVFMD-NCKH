3b360bec5ac0be9183b123c8b7ea77dcfe9d72c7
DESKTOP-KB2KJ9F\HoangHung <hoangkyo992@gmail.com>
2020-02-08 09:36:12 +0700

    update bug 5-6-7-8-9-10

--- appView/src/pages/WEB/nhan-su-syll-modal/nhan-su-syll-modal.ts
+++ appView/src/pages/WEB/nhan-su-syll-modal/nhan-su-syll-modal.ts
@@ -101,6 +101,10 @@ export class NhanSuSYLLModalPage extends DetailPage {
             self.ThuTruongCoQuan = ko.observable(item.ThuTruongCoQuan || "");
             self.DienThoaiThuTruong = ko.observable(item.DienThoaiThuTruong || "");
             self.DiaChiCoQuan = ko.observable(item.DiaChiCoQuan || "");
+            self.NgayKy_Ngay = ko.observable(item.NgayKy_Ngay || "");
+            self.NgayKy_Thang = ko.observable(item.NgayKy_Thang || "");
+            self.NgayKy_Nam = ko.observable(item.NgayKy_Nam || "");
+            self.NgayKy_ChuKy = ko.observable(item.NgayKy_ChuKy || "");
            
             self.ListTrinhDoChuyenMon = ko.observableArray(ko.utils.arrayMap(item.ListTrinhDoChuyenMon || [{}, {}, {}], function (nn) {
                 return {
