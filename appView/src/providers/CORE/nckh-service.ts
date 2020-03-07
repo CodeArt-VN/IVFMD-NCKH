43a3462a745bd1101bad0c68292cb991c1885e95
DESKTOP-KB2KJ9F\HoangHung <hoangkyo992@gmail.com>
2020-03-07 17:06:47 +0700

    resizable grid

--- appView/src/providers/CORE/nckh-service.ts
+++ appView/src/providers/CORE/nckh-service.ts
@@ -120,11 +120,11 @@ export class NCKHServiceProvider {
 
         var ptableGroupConrtrol =
             $('<div class="group_controls" style="position:absolute;top:-24px;right:0;border:1px solid red">' +
-                  '<div class="fieldgroup_controls">' +
-                      '<button style="line-height:20px" class="remove"><i class="fa fa-minus"></i> Xóa</button>' +
-                      '<button style="line-height:20px" class="clone"><i class="fa fa-plus"></i> Thêm</button>' +
-                  '</div>' +
-              '</div>');
+                '<div class="fieldgroup_controls">' +
+                '<button style="line-height:20px" class="remove"><i class="fa fa-minus"></i> Xóa</button>' +
+                '<button style="line-height:20px" class="clone"><i class="fa fa-plus"></i> Thêm</button>' +
+                '</div>' +
+                '</div>');
         $(".ptable").mouseenter(function (event) {
             try {
                 var ptable = $(event.currentTarget),
@@ -150,10 +150,10 @@ export class NCKHServiceProvider {
 
         var pblockConrtrol =
             $('<div class="group_controls" style="position:absolute;top:0;right:0;border:1px solid red">' +
-                  '<div class="fieldgroup_controls">' +
-                      '<button style="line-height:20px" class="clone"><i class="fa fa-plus"></i> Thêm</button>' +
-                  '</div>' +
-              '</div>');
+                '<div class="fieldgroup_controls">' +
+                '<button style="line-height:20px" class="clone"><i class="fa fa-plus"></i> Thêm</button>' +
+                '</div>' +
+                '</div>');
         $(".pblock").mouseenter(function (event) {
             try {
                 var pblock = $(event.currentTarget);
@@ -179,10 +179,10 @@ export class NCKHServiceProvider {
 
         var prowConrtrol =
             $('<div class="group_controls" style="position:absolute;top:0;right:0;border:1px solid red">' +
-                  '<div class="fieldgroup_controls">' +
-                      '<button style="line-height:20px" class="remove"><i class="fa fa-minus"></i> Xóa</button>' +
-                  '</div>' +
-              '</div>');
+                '<div class="fieldgroup_controls">' +
+                '<button style="line-height:20px" class="remove"><i class="fa fa-minus"></i> Xóa</button>' +
+                '</div>' +
+                '</div>');
         $(".pblock").on("mouseenter", ".prow", function (event) {
             try {
                 var pblock = $(event.currentTarget);
@@ -206,6 +206,89 @@ export class NCKHServiceProvider {
                 group_controls = pblock.find(".group_controls");
             group_controls.detach();
         });
+
+        var tables = document.getElementsByClassName('resize-grid');
+        for (var i = 0; i < tables.length; i++) {
+            resizableGrid(tables[i]);
+        }
+
+        function resizableGrid(table) {
+            var row = table.getElementsByTagName('tr')[0],
+                cols = row ? row.children : undefined;
+            if (!cols) return;
+            table.style.overflow = 'hidden';
+            for (var i = 0; i < cols.length; i++) {
+                var div = createDiv(2440);
+                cols[i].appendChild(div);
+                cols[i].style.position = 'relative';
+                setListeners(div);
+            }
+
+            function setListeners(div) {
+                var pageX, curCol, nxtCol, curColWidth, nxtColWidth;
+
+                div.addEventListener('mousedown', function (e) {
+                    curCol = e.target.parentElement;
+                    nxtCol = curCol.nextElementSibling;
+                    pageX = e.pageX;
+                    var padding = paddingDiff(curCol);
+                    curColWidth = curCol.offsetWidth - padding;
+                    if (nxtCol)
+                        nxtColWidth = nxtCol.offsetWidth - padding;
+                });
+
+                div.addEventListener('mouseover', function (e) {
+                    e.target.style.borderRight = '2px solid #cbcbcb';
+                })
+
+                div.addEventListener('mouseout', function (e) {
+                    e.target.style.borderRight = '';
+                })
+
+                document.addEventListener('mousemove', function (e) {
+                    if (curCol) {
+                        var diffX = e.pageX - pageX;
+                        if (nxtCol)
+                            nxtCol.style.width = (nxtColWidth - (diffX)) + 'px';
+                        curCol.style.width = (curColWidth + diffX) + 'px';
+                    }
+                });
+
+                document.addEventListener('mouseup', function (e) {
+                    curCol = undefined;
+                    nxtCol = undefined;
+                    pageX = undefined;
+                    nxtColWidth = undefined;
+                    curColWidth = undefined
+                });
+            }
+
+            function createDiv(height) {
+                var div = document.createElement('div');
+                div.style.top = '0';
+                div.style.right = '0';
+                div.style.width = '5px';
+                div.style.position = 'absolute';
+                div.style.cursor = 'col-resize';
+                div.style.userSelect = 'none';
+                div.style.height = height + 'px';
+                return div;
+            }
+
+            function paddingDiff(col) {
+                if (getStyleVal(col, 'box-sizing') == 'border-box') {
+                    return 0;
+                }
+                var padLeft = getStyleVal(col, 'padding-left');
+                var padRight = getStyleVal(col, 'padding-right');
+                return (parseInt(padLeft) + parseInt(padRight));
+
+            }
+
+            function getStyleVal(elm, css) {
+                return (window.getComputedStyle(elm, null).getPropertyValue(css))
+            }
+        };
     }
     observableSimpleArray(arr, samples: Array<string>) {
         if ((arr == null || arr == undefined || arr.length == 0) && samples != null && samples.length > 0)
