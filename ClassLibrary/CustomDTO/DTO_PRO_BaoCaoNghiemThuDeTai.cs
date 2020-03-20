cd26e602a7fbc3115a8403230202af915185f87f
Hoang Hung <hoangkyo992@gmail.com>
2020-03-21 00:31:52 +0700

    BL NghiemThuDeTai

---
+++ ClassLibrary/CustomDTO/DTO_PRO_BaoCaoNghiemThuDeTai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModel
+{
+    public partial class DTO_PRO_BaoCaoNghiemThuDeTai
+    {
+        public List<DTO_PRO_BaoCaoNghiemThuDeTai_CanBo> ListCanBo { get; set; }
+    }
+    public class DTO_PRO_BaoCaoNghiemThuDeTai_CanBo
+    {
+        public string HocHamHoTen { get; set; }
+        public string ChiuTrachNhiem { get; set; }
+        public string DienThoai { get; set; }
+        public string Email { get; set; }
+    }
+}
